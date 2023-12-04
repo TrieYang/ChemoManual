7907a046dad3ea013c8e31f22150dd98365fe33d
Trie <p2e1r@students.cs.ubc.ca>
2023-12-04 16:21:49 +0800

    Initial Commit

---
+++ ChemoManual/ChemoManualApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemoManualApp.swift
+//  ChemoManual
+//
+//  Created by Trie Yang on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct ChemoManualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
